e957577a2b29bca38b94f225d9603f38bf34e917
EI CHANUDOM <chanudomei122@gmail.com>
2024-08-14 23:46:25 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ViteSSG } from 'vite-plugin-ssg';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteSSG({
-      // Entry point to your app
-      entry: 'src/main.jsx',
-      // Define routes to be pre-rendered
-      routes: ['/'],
-    }),
-  ],
-});
+  plugins: [react()],
+})
